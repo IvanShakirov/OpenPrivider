2988053219fbedafdea02081283775a2f6558313
IvanShakirov <ivan.v.shakirov@gmail.com>
2019-05-15 19:17:37 +0700

    Aaaaa

--- script.js
+++ script.js
@@ -1,97 +1,127 @@
-function filling_table(api_answer,index) {
-    $('table tbody tr').remove();
-    for(var i=0; i<index.length;i++)
-    {
-    $('table tbody').append($("<tr>"));
-    $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].id));
-    $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].owner.login));
-    $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].name));
-    $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].description));
-    }
+function filling_table() {
+    since=since||0;
+    var index;
+    var link ='https://api.github.com/repositories?since=' + since;
+    $.ajax({
+        url: link,
+        type: "GET",
+        set: 200,
+        success: function(resultData){
+            var api_answer=resultData;
+            index=name_filter(api_answer);
+            $('table tbody tr').remove();
+            for(var i=0; i<index.length;i++)
+            {
+                $('table tbody').append($("<tr>"));
+                $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].id));
+                $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].owner.login));
+                $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].name));
+                $('table tbody tr').eq(i).append($("<td>").text(api_answer[index[i]].description));
+            }
+            pagination(api_answer);
+        }
 
+    });
 }
+
 function name_filter(api_answer) {
-    var text='';
+    var text=$('.search input').val();
     var index=[];
     var k=0;
-    $('.search input').on('keyup', function () {
-        text = $(this).val();
-        k=0;
-        for( var i=0; i<api_answer.length; i++)
-        {
-            if (api_answer[i].owner.login.indexOf(text)!==-1 ){
-                index[k]=i;
-                k++;
-            }
-        }
-        filling_table(api_answer,index);
-        index.length=0;
-    });
-    if(text===''){
+    if(text!=='') {
+           for (var i = 0; i < api_answer.length; i++) {
+                if (api_answer[i].owner.login.indexOf(text) !== -1) {
+                    index[k] = i;
+                    k++;
+                     }
+                }
+    }
+    else{//для первого прохождения и при пустом слоте с именем
         for( var i=0; i<api_answer.length ; i++)
         {
             index[i]=i;
         }
-        filling_table(api_answer,index);
-        index.length=0;
     }
+    return(index);
 }
-function pagination() {
+function pagination_update () {
+    since=0;
+    filling_table();
+    StartPageIndex=1;
+    PrevSince.length=0;
+    PrevSince[0]=1;
+}
+function pagination(api_answer) {
     var table = '#table';
-    $('#maxRows').on('change',function(){
-        $('.pagination').html('');
-        var trnum = 0 ;
-        var maxRows = parseInt($(this).val());
-        var totalRows = $(table+' tbody tr').length;
-        $(table+' tr:gt(0)').each(function(){
-            trnum++;
-            if (trnum > maxRows ){
-                $(this).hide();
-            }if (trnum <= maxRows ){$(this).show();}
-        });
-        if (totalRows > maxRows){
-            var pagenum = Math.ceil(totalRows/maxRows);
-            for (var i = 1; i <= pagenum ;){
-                $('.pagination').append('<li class="pagination-item page-link" data-page="'+i+'">\
-								      <span>'+ i++ +'<span class="sr-only">(current)</span></span>\
-								    </li>').show();
-            }
+    $('.pagination').html('');
+    var Trnum = 0;
+    var MaxRows = parseInt($('#maxRows').val());
+    var TotalRows = $(table + ' tbody tr').length;
+    $(table + ' tr:gt(0)').each(function () {
+        Trnum++;
+        if (Trnum > MaxRows) {
+            $(this).hide();
         }
-        $('.pagination li:first-child').addClass('active');
-        $('.pagination li').on('click',function(){
-            var pageNum = $(this).attr('data-page');
-            var trIndex = 0 ;
-            $('.pagination li').removeClass('active');
-            $(this).addClass('active');
-            $(table+' tr:gt(0)').each(function(){
-                trIndex++;
-                if (trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
-                    $(this).hide();
-                }else {$(this).show();}
-            });
-        });
+        if (Trnum <= MaxRows) {
+            $(this).show();
+        }
     });
-    // $(function(){
-    //     $('table tr:eq(0)').prepend('<th> ID </th>');
-    //     var id = 0;
-    //     $('table tr:gt(0)').each(function(){
-    //         id++;
-    //         $(this).prepend('<td>'+id+'</td>');
-    //     });
-    // })
-}
-function f() {
-var a;
-$.ajax({
-   url: 'https://api.github.com/repositories',
-    type: "GET",
-    success: function(resultData){
-       a=resultData;
-       console.log(a);
-     //  filling_table(resultData);
-        name_filter(resultData);
-        pagination();
+    if (TotalRows > MaxRows) {
+        var PageNum = Math.ceil(TotalRows / MaxRows);
+        for (var i = StartPageIndex; i <= PageNum + StartPageIndex -1;) {
+            $('.pagination').append('<li class="pagination-item page-link" data-page="' + i + '">\
+								      <span>' + i++ + '<span class="sr-only">(current)</span></span>\
+								    </li>').show();
+        }
     }
-});
+    $('.pagination li:first-child').addClass('active');
+    if(StartPageIndex>1)
+    {
+        $('.pagination').prepend('<a class="pagination-item page-link "  > <span aria-hidden="true">...</span> ' +
+            '<span class="sr-only">Previous</span> </a>');
+    }
+    $('.pagination').append('<a class="pagination-item page-link "  > <span aria-hidden="true">...</span> ' +
+        '<span class="sr-only">Previous</span> </a>');
+    $('.pagination').prepend('<li class="pagination-item page-link " data-sign="Previous" > <span aria-hidden="true">&laquo;</span> ' +
+        '<span class="sr-only">Previous</span> </li>');
+    $('.pagination').append('<li class="pagination-item page-link active" data-sign="Next"> <span aria-hidden="true data-page="Next">&raquo;</span> ' +
+        '<span class="sr-only">Next</span> </li');
+    if(StartPageIndex>1){
+        $('.pagination li:first-child').css('pointer-events','VisiblePainted');
+    }else {
+        $('.pagination li:first-child').css('color','#708289')
+    }
+    $('.pagination li').on('click', function () {
+        $(window).scrollTop(0);
+        var TrIndex = 0;
+        var pageNum = +$(this).attr('data-page')-StartPageIndex+1;
+        $('.pagination li').removeClass('active');
+        $(this).addClass('active');
+        if ($(this).attr('data-sign') === "Previous") {
+            since = PrevSince[StartPageIndex];
+            StartPageIndex--;
+            filling_table()
+        }  if ($(this).attr('data-sign')=== "Next") {
+            StartPageIndex++;
+            since = api_answer[MaxRows-1].id+1;
+            PrevSince[StartPageIndex]=api_answer[0].id -1;
+            filling_table();
+            console.log(api_answer);
+        }
+            $(table + ' tr:gt(0)').each(function a() {
+                TrIndex++;
+
+                if (TrIndex > (MaxRows * pageNum) || TrIndex <= ((MaxRows * pageNum) - MaxRows)) {
+                    $(this).hide();
+                } else {
+                    $(this).show();
+                }
+            });
+    });
 
 }
+
+
+var since=0; //некрасиво, надо убрать, но я хз как
+var PrevSince=[];PrevSince[0]=1;    //некрасиво, надо убрать, но я хз как
+var StartPageIndex=1;   //некрасиво, надо убрать, но я хз как
